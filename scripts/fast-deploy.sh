32e68072099cfd513b048b92aed87982363dfbcd
Andrew Smith <andrew.smith.recruiting@gmail.com>
2024-12-23 13:27:28 -0800

    [fix]: make fast deploy path config

--- scripts/fast-deploy.sh
+++ scripts/fast-deploy.sh
@@ -2,12 +2,13 @@ set -e
 
 # ATTENTION: Fill this in
 PATH_TO_YOUR_YAML_FILE="/home/andrew/Documents/projects/ai2-beaker-template/beaker/yaml/beaker-config-distrib.yaml"
+IMAGE_NAME="dna-seq-models"
+IMAGE_ID=$(openssl rand -hex 3)
 
 sudo ./scripts/build.sh image-name
 
-IMAGE_ID=$(openssl rand -hex 3)
-beaker image create --name image-name-$IMAGE_ID image-name
+beaker image create --name $IMAGE_NAME-$IMAGE_ID $IMAGE_NAME
 
-sed -i.backup "s/image-name-\([a-zA-Z0-9]*\)/image-name-${IMAGE_ID}/g" $PATH_TO_YOUR_YAML_FILE
+sed -i.backup "s/$IMAGE_NAME-\([a-zA-Z0-9]*\)/$IMAGE_NAME-${IMAGE_ID}/g" $PATH_TO_YOUR_YAML_FILE
 
 beaker experiment create $PATH_TO_YOUR_YAML_FILE
